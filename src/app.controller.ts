92ee88c53c1da86fdbde225b6f6df07f2ff75cfa
박준규 <junju404@naver.com>
2024-04-10 01:21:08 +0900

    Feat : Link chat-gpt with roblox studio as post method

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
-import { Controller, Get } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { AppService } from './app.service';
+import { ChatGptService } from './chat-gpt/chat-gpt.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+    private readonly appService: AppService,
+    private readonly chatGptService: ChatGptService,
+  ) {}
 
-  @Get('/hello')
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('/chat')
+  async getChatResponse(
+    @Body() body: { prompt: string },
+  ): Promise<{ response: string }> {
+    console.log('Received Prompt:', body.prompt);
+    const response = await this.chatGptService.generateResponse(body.prompt);
+    return { response };
   }
 }
