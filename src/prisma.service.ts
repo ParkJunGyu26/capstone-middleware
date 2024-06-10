0a5d16b88b705a19b349c8df6fd49585a7bfdc33
박준규 <junju404@naver.com>
2024-06-10 11:18:00 +0900

    Chore : prisma logger

--- src/prisma.service.ts
+++ src/prisma.service.ts
@@ -6,6 +6,12 @@ export class PrismaService
   extends PrismaClient
   implements OnModuleInit, OnModuleDestroy
 {
+  constructor() {
+    super({
+      log: ['query', 'info', 'warn', 'error'], // 로그 레벨 설정
+    });
+  }
+
   async onModuleInit() {
     await this.$connect();
   }
